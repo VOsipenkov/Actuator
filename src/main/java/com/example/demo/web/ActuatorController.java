dccdd71247eefe611765ecc0731c963072401815
VOsipenkov <jumpViaOcean@gmail.com>
2022-09-20 23:32:39 +0300

    Added host and port info

--- src/main/java/com/example/demo/web/ActuatorController.java
+++ src/main/java/com/example/demo/web/ActuatorController.java
@@ -1,7 +1,21 @@
 package com.example.demo.web;
 
+import lombok.RequiredArgsConstructor;
+import org.springframework.core.env.Environment;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.net.InetAddress;
+
 @RestController
+@RequiredArgsConstructor
 public class ActuatorController {
+    private final Environment environment;
+
+    @GetMapping(value = "/host")
+    public String getHost() throws Exception {
+        var port = environment.getProperty("local.server.port");
+        var host = InetAddress.getLocalHost().getHostAddress();
+        return "host: " + host + " port:" + port;
+    }
 }
